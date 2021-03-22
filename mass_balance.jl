583401a9a50820e4d4187dd2d75e78476c8d8f5f
Jordi Bolibar <jordib155@gmail.com>
2021-03-22 22:03:15 +0100

    Training working with data plots

--- mass_balance.jl
+++ mass_balance.jl
@@ -36,6 +36,9 @@ function toy_MB(α, μ, P, T)
     return MB
 end
 
+# Container to track the losses
+losses = Float32[]
+
 # Callback to show the loss during training
 callback(l) = begin
     push!(losses, l)
@@ -49,19 +52,18 @@ function hybrid_train!(loss, ps_Up, ps_Ut, data, opt)
     ps_Up = Params(ps_Up)
     ps_Ut = Params(ps_Ut)
     for batch in data
-      print("batch: ", batch, "\n")
       # back is a method that computes the product of the gradient so far with its argument.
       train_loss_Up, back_Up = Zygote.pullback(() -> loss(batch...), ps_Up)
-      train_loss_Ut, back_Up = Zygote.pullback(() -> loss(batch...), ps_Ut)
+      train_loss_Ut, back_Ut = Zygote.pullback(() -> loss(batch...), ps_Ut)
       # Callback to track the training
-      callback(loss)
+      callback(train_loss_Up)
       # Apply back() to the correct type of 1.0 to get the gradient of loss.
-      gs_Up = back(one(train_loss_Up))
-      gs_Ut = back(one(train_loss_Ut))
+      gs_Up = back_Up(one(train_loss_Up))
+      gs_Ut = back_Ut(one(train_loss_Ut))
       # Insert what ever code you want here that needs gradient.
       # E.g. logging with TensorBoardLogger.jl as histogram so you can see if it is becoming huge.
-      update!(opt, ps_Up, gs_Up)
-      update!(opt, ps_Ut, gs_Ut)
+      Flux.update!(opt, ps_Up, gs_Up)
+      Flux.update!(opt, ps_Ut, gs_Ut)
       # Here you might like to check validation set accuracy, and break out to do early stopping.
     end
   end
@@ -79,59 +81,73 @@ function hybrid_train!(loss, ps_Up, ps_Ut, data, opt)
 leakyrelu(x, a=0.01) = max(a*x, x)
 
 # Define the networks 1->5->5->5->1
-Up = FastChain(
-    FastDense(1,5, leakyrelu), 
-    FastDense(5,5, leakyrelu), 
-    FastDense(5,5, leakyrelu), 
-    FastDense(5,1)
+Up = Chain(
+    Dense(1,5, leakyrelu), 
+    Dense(5,5, leakyrelu), 
+    Dense(5,5, leakyrelu), 
+    Dense(5,1)
 )
 
-Ut = FastChain(
-    FastDense(1,5,leakyrelu), 
-    FastDense(5,5, leakyrelu), 
-    FastDense(5,5, leakyrelu), 
-    FastDense(5,1)
+Ut = Chain(
+    Dense(1,5, leakyrelu), 
+    Dense(5,5, leakyrelu), 
+    Dense(5,5, leakyrelu), 
+    Dense(5,1)
 )
+
 
 # # Get the initial parameters, first is linear decay
 # p_P = [rand(Float32); initial_params(Up)]
 # p_T = [rand(Float32); initial_params(Ut)]
 
 # We define an optimizer
-opt = RMSProp(0.002, 0.95)
+#opt = RMSProp(0.002, 0.95)
+opt = ADAM()
 
 # We get the model parameters to be trained
 ps_Up = Flux.params(Up)
 ps_Ut = Flux.params(Ut)
 
+sqnorm(x) = sum(abs2, x)
+
 # We determine the loss function
 function loss(x, y)
     # Start with a regularization on the network
-    # We evaluate the MB as the combination of Accumulation - Ablation
-    l = sqrt(mse(Up(x) - Ut(x), y; agg=mean))
+    # We evaluate the MB as the combination of Accumulation - Ablation with L2
+    l = sqrt(Flux.Losses.mse(Up(x[1,:]) - Ut(x[2,:]), y; agg=mean)) + sum(sqnorm, Flux.params(Up)) + sum(sqnorm, Flux.params(Ut))
 
     return l
 end
 
 #######  We generate toy data to train the model  ########
 snowfall_toy = rand(0.0f0:10.0f0, 500)
-temperature_toy = 10f0*sin.(1:500)
+temperature_toy = 10f0*sin.((1:500)./10)
 forcings = hcat(snowfall_toy, temperature_toy)
 α(P) = P.^(1.1)
 μ(T) = T.^(1.3)
 MB_toy = toy_MB(α, μ, snowfall_toy, temperature_toy)
 
-X = hcat(snowfall_toy, temperature_toy)'
-Y = copy(MB_toy)
+# Plot the toy dataset 
+l = @layout [a b; c]
+p1 = plot(1:500, snowfall_toy, label="Snowfall", color="midnightblue")
+p2 = plot(1:500, temperature_toy, label="Temperature", color="darkred")
+hline!(p2, [0], c="black", label="")
+p3 = plot(1:500, MB_toy, label="Mass balance")
+hline!(p3, [0], c="black", label="")
+plot(p1,p2,p3,layout=l)
 
-data = Flux.Data.DataLoader(X, Y, batchsize=128)
+
+X = vcat(snowfall_toy', temperature_toy')
+Y = collect(MB_toy')
+
+batch_size = 1
+data = Flux.Data.DataLoader((X, Y), batchsize=batch_size)
 
 #data = (X, Y)
 
 # We train the mass balance hybrid model
-number_epochs = 100
+number_epochs = 4
 @epochs number_epochs hybrid_train!(loss, ps_Up, ps_Ut, data, opt)
-
 
 
 #end
