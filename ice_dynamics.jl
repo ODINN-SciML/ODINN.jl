3a2f8aad7b02bdb2e8457784e62a3730442d7c55
Jordi <jordi.bolibar@gmail.com>
2021-04-15 18:05:31 +0200

    Ice flux fixed, with forward modelling
    Ice flux correctly computed with the gradient squared norm. Forward modelling working in a loop with time differentiation. Units of flux are still wrong, giving weird results. There are still some things to fix.

--- ice_dynamics.jl
+++ ice_dynamics.jl
@@ -52,11 +52,6 @@ function four_point_avg!(Nodes_4p_avg::Nodes{Primal}, dualNodes::Nodes{Dual}, i,
     Nodes_4p_avg[i,j] = (dualNodes[i,j] + dualNodes[i+1,j] + dualNodes[i,j+1] + dualNodes[i+1,j+1])/4
 end
 
-# 4 point average of Dual Edges to a single Primal Node
-function four_point_avg!(Nodes_4p_avg::Nodes{Primal}, dualEdges::Edges{Dual}, i,j)
-    Nodes_4p_avg[i,j] = (dualEdges.u[i,j] + dualEdges.u[i,j+1] + dualEdges.v[i,j] + dualEdges.v[i+1,j])/4
-end
-
 # Performs a 4 point average of Node values of the Dual grid to the Primal grid
 function dual2primal_4p_avg!(primalNodes::Nodes{Primal}, dualNodes::Nodes{Dual})
     for i in 1:size(primalNodes)[1]
@@ -66,56 +61,37 @@ function dual2primal_4p_avg!(primalNodes::Nodes{Primal}, dualNodes::Nodes{Dual})
     end
 end
 
-# Performs a 4 point average of Edge values of the Dual grid to the Primal grid
-function dual2primal_4p_avg!(primalNodes::Nodes{Primal}, dualEdges::Edges{Dual})
-    for i in 1:size(primalNodes)[1]
-        for j in 1:size(primalNodes)[2]
-            four_point_avg!(primalNodes, dualEdges, i,j)
+# Squared norm of a gradient field
+function ∇_norm²(gradEdges::Edges{Dual}, dualNodes::Nodes{Dual})
+    ∇_norm² = Nodes(Primal, dualNodes)
+    for i in 1:size(∇_norm²)[1]
+        for j in 1:size(∇_norm²)[2]
+            # 2-point mean of gradients
+            u̅ = mean([gradEdges.u[i,j], gradEdges.u[i,j+1]])
+            v̅ = mean([gradEdges.v[i,j], gradEdges.v[i+1,j]])
+            # Squared norm
+            ∇_norm²[i,j] = norm([u̅, v̅])^2
         end
     end
-end
 
-function SIA_hybrid(params, Ua, Un, Uc)
-
-
-    return u
-end
-
-function SIA!(du, u, p, t)
-    A, ρ, g, n = p
-    H, ∂S∂x, ∂S∂y = u
-    # Flux in x axis
-    du[1] = ((2*A)/(n+2))*((ρ*g*H)^n)*(H^2)*∂S∂x
-    # Flux in y axis
-    du[2] = ((2*A)/(n+2))*((ρ*g*H)^n)*(H^2)*∂S∂y
+    return ∇_norm²
 end
 
 # Compute diffusivities on u and v
-function diffusivities(vars, p)
+function diffusivity(vars, p)
     A, ρ, g, n = p
-    H, Su, Sv = vars
-    D_u = (2/(n+2)).*((ρ.*g.*H).^n).*A.*(H.^2).*(Su.^2)
-    D_v = (2/(n+2)).*((ρ.*g.*H).^n).*A.*(H.^2).*(Sv.^2)
+    H, ∇S_norm² = vars
+    D = ((2*A)/(n+2)).*((ρ*g.*H).^n).*(H.^2).*(∇S_norm².^(n-1))
     # Diffusivities Nodes
-    D_pn_u = Nodes(Primal,dem_dn)
-    D_pn_v = Nodes(Primal,dem_dn)
-    D_pn_u .= D_u
-    D_pn_v .= D_v
+    D_pn = Nodes(Primal,dem_dn)
+    D_pn .= D
 
-    return D_pn_u, D_pn_v
-end
-
-function u_s_test!(du, u, p, idx, t)
-    A, ρ, g, n = p
-    H, ∂S∂x, ∂S∂y = u
-    du[idx[1], idx[2]] = abs(((2*A)/(n+2))*((ρ*g*H)^n)*H*∂S∂x*∂S∂x^(n-1)) + abs(((2*A)/(n+2))*((ρ*g*H)^n)*H*∂S∂y*∂S∂y^(n-1))
+    return D_pn
 end
 
 ###############################################################
 ###########################  MAIN #############################
 ###############################################################
-
-grid_size = 50 #m
 
 # Load the HDF5 file with Harry's simulated data
 argentiere_f = h5open("/Users/Bolib001/Desktop/Jordi/Data/Toy ice dynamics/Argentiere_2003-2100_aflow2e-16_50mres.h5", "r")
@@ -133,9 +109,12 @@ hm03 = heatmap(argentiere.vel[:,:,15], c = :speed, title="Ice velocities")
 hm0 = plot(hm01,hm02,hm03, layout=(3,1), aspect_ratio=:equal, size=(500,1000))
 display(hm0)
 
+# Initial parameters
 arg_dem = copy(argentiere.bed) .+ copy(argentiere.thick[:,:,1]) 
-arg_thick = copy(argentiere.thick[:,:,1]) 
 p = (2e-16, 900, 9.81, 3) # A, ρ, g, n
+# Spatial and temporal differentials
+Δx = 50 #m
+Δt = 0.05 #years
 
 ####################################
 ##### Create staggered grid  #######
@@ -156,88 +135,68 @@ thick_pn = Nodes(Primal, thick_dn)
 dem_dn = Nodes(Dual,size(arg_dem))
 dem_dn .= arg_dem
 dem_pn = Nodes(Primal,dem_dn)
-#dem_pn .= arg_dem[1:end-1,1:end-1,1]
 
-# Surface gradients
-grad_dn = Nodes(Dual,dem_dn)
-grad_pn_u = Nodes(Primal,grad_dn)
-grad_pn_v = Nodes(Primal,grad_dn)
+for t in 0:Δt:10
 
-#####################################################
-#### 1 - Calculate diffusivities in Primal grid  ####
-#####################################################
-println("Calculating diffusivities in Primal Nodes \n")
+    #####################################################
+    #### 1 - Calculate diffusivities in Primal grid  ####
+    #####################################################
+    #println("Calculating diffusivities in Primal Nodes \n")
 
-# 1.1: 4-point average of ice thickness from Dual to Primal  
-dual2primal_4p_avg!(thick_pn, thick_dn)
+    # 1.1: 4-point average of ice thickness from Dual to Primal  
+    dual2primal_4p_avg!(thick_pn, thick_dn)
+    dual2primal_4p_avg!(dem_pn, dem_dn)
 
-# 1.2: 2-point central estimate of surface gradient from Dual Nodes to Dual Edges
-grad_de = grad(dem_dn)
+    # 1.2: 2-point central estimate of surface gradient from Dual Nodes to Dual Edges
+    ∇_S_de = grad(dem_dn)/Δx
 
-# 1.3: 2-point average of gradients from Dual Edges to Primal Nodes
-#dual2primal_4p_avg!(grad_pn, grad_de)
-grid_interpolate!(grad_pn_u, grad_de.u)
-grid_interpolate!(grad_pn_v, grad_de.v)
+    # 1.3: 2-point average of squared norm of gradients from Dual Edges to Primal Nodes
+    ∇S_norm² = ∇_norm²(∇_S_de, dem_dn)
 
-# 1.4: Calculate diffusivities on Primal Nodes
-vars = thick_pn, grad_pn_u, grad_pn_v
-D_pn_u, D_pn_v = diffusivities(vars, p)
+    # 1.4: Calculate diffusivities on Primal Nodes
+    vars = thick_pn, ∇S_norm²
+    D_pn = diffusivity(vars, p)
 
-#####################################################
-####   2 - Update ice thicknesses on Dual Nodes  ####
-#####################################################
-println("Calculating flux divergence on Dual Nodes \n")
+    #####################################################
+    ####   2 - Update ice thicknesses on Dual Nodes  ####
+    #####################################################
+    #println("Calculating flux divergence on Dual Nodes \n")
 
-# 2.1: 2-point average of diffusivity from Primal Nodes to Dual Edges
-D_de = Edges(Dual, dem_dn)
-grid_interpolate!(D_de.u, D_pn_u)
-grid_interpolate!(D_de.v, D_pn_v)
+    # 2.1: 2-point average of diffusivity from Primal Nodes to Dual Edges
+    D_de = Edges(Dual, ∇_S_de)
+    grid_interpolate!(D_de.u, D_pn)
+    grid_interpolate!(D_de.v, D_pn)
 
-# 2.2: 2-point central estimate of surface gradient from Dual Nodes to Dual Edges
-grad_de
+    # 2.2: 2-point central estimate of surface gradient from Dual Nodes to Dual Edges
+    grad_de
 
-# 2.3: 2-point central estimate of flux divergence from Dual Edges to Dual Nodes
-# Computation of flux (F) on Dual Edges
-F_de = Edges(Dual, D_de)
-F_e_u = D_de.u.*grad_de.u # F = D*∂h/∂x 
-F_e_v = D_de.v.*grad_de.v # F = D*∂h/∂y
-F_de.u .= F_e_u
-F_de.v .= F_e_v
-# Computation of flux divergence on Dual Nodes
-divergence!(F_dn, F_de)
-#vel_dn = Nodes(Dual, F_dn)
-vel_dn = F_dn.data./thick_dn.data
+    # 2.3: 2-point central estimate of flux divergence from Dual Edges to Dual Nodes
+    # Computation of flux (F) on Dual Edges
+    F_de = Edges(Dual, D_de)
+    F_de.u .= D_de.u.*∇_S_de.u # F = D*∂S/∂x 
+    F_de.v .= D_de.v.*∇_S_de.v # F = D*∂S/∂y
+    # Computation of flux divergence on Dual Nodes
+    divergence!(F_dn, F_de)
 
-hm11 = heatmap(F_dn.data, title="Flux divergence")
-hm12 = heatmap(thick_dn.data, c = :ice, title="Ice thickness")
-hm13 = heatmap(vel_dn, c = :speed, title="Ice velocities")
-hm1 = plot(hm11,hm12,hm13, layout=(3,1), aspect_ratio=:equal, size=(500,1000))
-display(hm1)
+    # Compute ice flux for Δt
+    F_dn *= Δt
+    F_dn /= 1000
+    # Apply ice flux
+    thick_dn +=F_dn
+    dem_dn +=F_dn
 
-### Apply flux on ice thicknesses  ####
+    #vel_dn = Nodes(Dual, F_dn)
+    vel_dn = F_dn.data./thick_dn.data
 
-thick_dn += F_dn
-dem_dn += F_dn
+    # Plot glacier evolution each year
+    if(t%1 == 0)
+        println(t)
+        hm11 = heatmap(F_dn.data, title="Flux divergence")
+        hm12 = heatmap(thick_dn.data, c = :ice, title="Ice thickness")
+        hm13 = heatmap(vel_dn, c = :speed, title="Ice velocities")
+        hm1 = plot(hm11,hm12,hm13, layout=(3,1), aspect_ratio=:equal, size=(500,1000), plot_title=t)
+        display(hm1)
+    end
 
-# ∇_diffusion = laplacian(dem_dn)
-# S_dem = grad(dem_dn)
-# println("S_dem: ", S_dem)
-# D = u_s(thick_de, p).*(S_dem.^2)
-# #display(heatmap(D.*∇_diffusion))
-
-# p_∇ = plot(heatmap(∇_diffusion))
-# p_grad = plot(heatmap(S_dem))
-# println("Size D: ", size(D))
-# println("Size ∇_diffusion: ", size(∇_diffusion))
-# #plot_∇ = plot(p_∇, p_grad, layout=l1)
-# #display(p_grad)
-
-# dem_pn += D.*∇_diffusion
-# thick_pn += D.*∇_diffusion
-# #println("Year: ", year)
-# hm = heatmap(arg_thick, clim=(0,350))
-# #hm = heatmap(arg_dem, clim=(1000,4000))
-# display(hm)
-
-
+end
 
