c0a654b87d1c606cc07e1397543db79c18ef7265
Jordi Bolibar <jordi.bolibar@gmail.com>
2022-02-10 16:56:24 +0000

    Tiny fix

--- scripts/toy_model/ODINN.jl
+++ scripts/toy_model/ODINN.jl
@@ -10,10 +10,11 @@ SINDy (Brunton et al., 2016).
 ###########  ACTIVATE ENVIRONMENT  ############
 ###############################################
 
-cd(@__DIR__)
-using Pkg 
-Pkg.activate("../../.");
-Pkg.instantiate()
+import Pkg
+# activate project
+Pkg.activate(dirname(Base.current_project()))
+# make sure all packages are precompiled
+Pkg.precompile()
 
 ################################################
 ############  PYTHON ENVIRONMENT  ##############
@@ -48,6 +49,7 @@ xr = pyimport("xarray")
 
 ## Environment and packages
 using Distributed
+using ProgressMeter
 const processes = 10
 
 if nprocs() < processes
@@ -58,22 +60,25 @@ println("Number of cores: ", nprocs())
 println("Number of workers: ", nworkers())
 
 @everywhere begin 
-    cd(@__DIR__)
-    using Pkg 
-    Pkg.activate("../../.");
-    Pkg.instantiate()
+    import Pkg
+    Pkg.activate(dirname(Base.current_project()))
+    Pkg.precompile()
 end
 
 @everywhere begin 
-using Plots; gr()
-using OrdinaryDiffEq
-using Tullio
-using RecursiveArrayTools
 using Statistics
 using LinearAlgebra
-using HDF5
+using Random
+using HDF5  
 using JLD
+using OrdinaryDiffEq
+using DiffEqFlux
+using Flux
+using Tullio
+using RecursiveArrayTools
 using Infiltrator
+using Plots
+using ProgressMeter
 using Dates # to provide correct Julian time slices 
 
 ###############################################
@@ -89,32 +94,35 @@ const minA = 3e-17
 const maxT = 1
 const minT = -25
 
+create_ref_dataset = true
+const noise = true # Add random noise to fake A law
+rng_seed() = MersenneTwister(123) # random seed
+
 ###############################################
 #############  ODINN LIBRARIES  ###############
 ###############################################
 
-### Iceflow forward model  ###
-# (includes utils.jl as well)
-include("iceflow.jl")
-
 cd(@__DIR__)
-root_dir = cd(pwd, "../..")
+root_dir = dirname(Base.current_project())
 
 ### Climate data processing  ###
-include(joinpath(root_dir, "scripts/helpers/climate.jl"))
+include("helpers/climate.jl")
 end # @everywhere
+
+### Iceflow forward model  ###
+# (includes utils.jl as well)
+include("helpers/iceflow.jl")
 
 cfg.initialize() # initialize OGGM configuration
 
 PATHS = PyDict(cfg."PATHS")  # OGGM PATHS
 home_dir = cd(pwd, "../../../..")
-PATHS["working_dir"] = joinpath(home_dir, "/Python/OGGM_data")  # Choose own custom path for the OGGM data
+PATHS["working_dir"] = joinpath(home_dir, "Python/OGGM_data")  # Choose own custom path for the OGGM data
 PARAMS = PyDict(cfg."PARAMS")
 
 # Multiprocessing 
-PARAMS["use_multiprocessing"] = false # Let's use multiprocessing for OGGM
-# ensemble = EnsembleSerial()
-ensemble = EnsembleDistributed() # multiprocessing for ODINN
+PARAMS["prcp_scaling_factor"], PARAMS["ice_density"], PARAMS["continue_on_error"]
+PARAMS["use_multiprocessing"] = true # Let's use multiprocessing for OGGM
 
 ###############################################################
 ###########################  MAIN #############################
@@ -127,18 +135,32 @@ rgi_ids = ["RGI60-11.03638", "RGI60-11.01450"]
 
 ### Initialize glacier directory to obtain DEM and ice thickness inversion  ###
 # Where to fetch the pre-processed directories
-gdirs = workflow.init_glacier_directories(rgi_ids, from_prepro_level=3, prepro_border=10) # TODO: change to Lilian's version in notebook (ODINN_MB.ipynb)
+# TODO: change to Lilian's version in notebook (ODINN_MB.ipynb)
+# use elevation band  flowlines
+base_url = ("https://cluster.klima.uni-bremen.de/~oggm/gdirs/oggm_v1.4/L1-L2_files/elev_bands")
+
+# Where to fetch the pre-processed directories
+# gdirs = workflow.init_glacier_directories(rgi_ids, from_prepro_level=2,
+#                                           prepro_border=10,
+#                                           prepro_base_url=base_url,
+#                                           prepro_rgi_version="62")
+
+# (@isdefined gdirs) || (gdirs = workflow.init_glacier_directories(rgi_ids, from_prepro_level=3, prepro_border=40)) 
+gdirs = workflow.init_glacier_directories(rgi_ids, from_prepro_level=3, prepro_border=40)
+
 
 gdir = gdirs[1]
+println("Path to the DEM:", gdir.get_filepath("dem"))
+
 
 # Obtain ice thickness inversion
 list_talks = [
     # tasks.glacier_masks,
     # tasks.compute_centerlines,
     # tasks.initialize_flowlines,
-    # tasks.compute_downstream_line,
+    tasks.compute_downstream_line,
     tasks.prepare_for_inversion,  # This is a preprocessing task
-    tasks.mass_conservation_inversion,  # This does the actual job
+    tasks.mass_conservation_inversion,  # This gdirsdoes the actual job
     # tasks.filter_inversion_output,  # This smoothes the thicknesses at the tongue a little
     tasks.distribute_thickness_per_altitude
 ]
@@ -146,29 +168,46 @@ for task in list_talks
     # The order matters!
     workflow.execute_entity_task(task, gdirs)
 end
+
 # Plot glacier domain
-graphics.plot_domain(gdirs)
-graphics.plot_distributed_thickness(gdir)     
+# graphics.plot_domain(gdirs)
+# graphics.plot_distributed_thickness(gdir)     
 
 glacier_gd = xr.open_dataset(gdir.get_filepath("gridded_data"))
 nx = glacier_gd.x.size # glacier extent
 ny = glacier_gd.y.size
 
+MBsandbox = pyimport("MBsandbox.mbmod_daily_oneflowline")
+
 ### Generate fake annual long-term temperature time series  ###
 # This represents the long-term average air temperature, which will be used to 
 # drive changes in the `A` value of the SIA
-temp_series =  fake_temp_series(t₁)
-A_series = []
-for temps in temp_series
-    push!(A_series, A_fake.(temps))
-end
-display(Plots.plot(temp_series, xaxis="Years", yaxis="Long-term average air temperature", title="Fake air temperature time series"))
-display(Plots.plot(A_series, xaxis="Years", yaxis="A", title="Fake A reference time series"))
+(@isdefined temp_series) || (const temp_series, norm_temp_series = fake_temp_series(t₁))
+# A_series = []
+# for temps in temp_series
+#     push!(A_series, A_fake.(temps))
+# end
+# display(Plots.plot(temp_series, xaxis="Years", yaxis="Long-term average air temperature", title="Fake air temperature time series"))
+# display(Plots.plot(A_series, xaxis="Years", yaxis="A", title="Fake A reference time series"))
 
 # Determine initial conditions
-H = glacier_gd.distributed_thickness.data # initial ice thickness conditions for forward model
-B = glacier_gd.topo.data - glacier_gd.distributed_thickness.data # bedrock
+(@isdefined H₀) || (const H₀ = glacier_gd.distributed_thickness.data) # initial ice thickness conditions for forward model
+(@isdefined B) || (const B = glacier_gd.topo.data - glacier_gd.distributed_thickness.data) # bedrock
 
 # Run forward model for selected glaciers
+if create_ref_dataset 
+    println("Generating reference dataset for training...")
+  
+    # Compute reference dataset in parallel
+    @everywhere solver = Ralston()
+    H_refs, V̄x_refs, V̄y_refs = generate_ref_dataset(temp_series, H₀)
+        
+        println("Saving reference data")
+    save(joinpath(root_dir, "data/H_refs_ODINN.jld"), "H_refs", H_refs)
+    save(joinpath(root_dir, "data/Vx_refs_ODINN.jld"), "Vx_refs", V̄x_refs)
+    save(joinpath(root_dir, "data/Vy_refs_ODINN.jld"), "Vy_refs", V̄y_refs)
+
+else
+
 
 
