489ed27d614c0e09a5421f5dc251f0e256dcacee
Jordi Bolibar <jordi.bolibar@gmail.com>
2022-02-16 11:47:14 +0000

    Combined loss function with H and V
    Now we can use more complex loss functions combining different metrics based on surface velocities and/or ice thickness.

--- scripts/toy_model/helpers/parameters.jl
+++ scripts/toy_model/helpers/parameters.jl
@@ -8,9 +8,10 @@ const minA = 3e-17
 const maxT = 1
 const minT = -25
 
-create_ref_dataset = true          # Run reference PDE to generate reference dataset
+create_ref_dataset = false          # Run reference PDE to generate reference dataset
 # loss_type = "H"                   # Loss function based on ice thickness
-loss_type = "V"                     # Loss function based on ice surface velocities
+# loss_type = "V"                   # Loss function based on ice surface velocities
+loss_type = "HV"                    # Combined loss function based on ice surface velocities and ice thickness
 const random_sampling_loss = false  # Use random subset of matrix samples for the loss
 const noise = true                  # Add random noise to fake A law
 rng_seed() = MersenneTwister(123)   # Random seed
