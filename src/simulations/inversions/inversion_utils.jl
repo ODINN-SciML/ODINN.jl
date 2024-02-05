7dbea93b38b5d2cee24f4f710327ac9775b2a72e
vivekag7 <vgajadhar7@gmail.com>
2024-02-05 16:01:11 +0000

    Added classical inversion for Parameter A

--- src/simulations/inversions/inversion_utils.jl
+++ src/simulations/inversions/inversion_utils.jl
@@ -1,16 +1,16 @@
-export run_inversion!
+export run₀
 
 """
     run_inversion!(simulation::Inversion)
 
-In-place run of classic inversion model. 
+Out-place run of classic inversion model. 
 """
-function run_inversion!(simulation::Inversion)
+function run₀(simulation::Inversion)
 
     enable_multiprocessing(simulation.parameters)
 
-    println("Running forward in-place PDE ice flow inversion model...\n")
-    inversion_params_list = @showprogress pmap((glacier_idx) -> invert_iceflow!(glacier_idx, simulation), 1:length(simulation.glaciers))
+    println("Running forward out-place PDE ice flow inversion model...\n")
+    inversion_params_list = @showprogress pmap((glacier_idx) -> invert_iceflow(glacier_idx, simulation), 1:length(simulation.glaciers))
 
     simulation.inversion = inversion_params_list
 
@@ -18,75 +18,139 @@ function run_inversion!(simulation::Inversion)
 
 end
 
-function invert_iceflow!(glacier_idx::Int, simulation::Inversion) 
-    #model = simulation.model
-    #params = simulation.parameters
+function invert_iceflow(glacier_idx::Int, simulation::Inversion) 
+    model = simulation.model
+    params = simulation.parameters
     glacier = simulation.glaciers[glacier_idx]
     
     glacier_id = isnothing(glacier.gdir) ? "unnamed" : glacier.rgi_id
     println("Processing glacier: ", glacier_id)
-      
-    # Trim observed velocity to match size predicted velocity 
-    #glacier.V=glacier.V[1:end-1, 1:end-1]
     
-    function objective_function(x,p)
-        glacier_idx = p[1]
-        model = p[2].model 
-        glacier = p[2].glaciers[glacier_idx] 
-        params = p[2].parameters
-        
-        model.iceflow.A=x[1]
-        model.iceflow.n=x[2]
-
-        # TODO: fix appropiate H and V
-        h_dim1, h_dim2 = size(glacier.H_glathida)
-        v_dim1, v_dim2 = size(glacier.V)
-
-        H = [1 * x[1] for i in 1:h_dim1, j in 1:h_dim2]
-        V = [1 * x[2] for i in 1:v_dim1, j in 1:v_dim2]
-       
-        # Extract the non-zero elements from observed data
-        non_zero_indices_H = glacier.H_glathida .!= 0
-        non_zero_indices_V = glacier.V .!= 0
-
-        # Calculate the MSE for H using only the non-zero elements
-        mse_H = mean(((glacier.H_glathida[non_zero_indices_H] .- H[non_zero_indices_H]) .^ 2))
-        
-        # Calculate the MSE for V
-        mse_V = mean(((glacier.V[non_zero_indices_V] .- V[non_zero_indices_V]) .^ 2))
-        
-        # Calculate weighted average of MSE's
-        count_H = sum(non_zero_indices_H)
-        count_V = sum(non_zero_indices_V)
+    nx, ny = glacier.nx, glacier.ny 
     
-        weighted_average_mse = (count_H * mse_H + count_V * mse_V) / (count_H + count_V)
+    model.iceflow.A=glacier.A
+    model.iceflow.n=glacier.n
+    model.iceflow.glacier_idx = glacier_idx
+    model.iceflow.H₀ = deepcopy(glacier.H₀)
+    model.iceflow.H  = deepcopy(glacier.H₀)
+    model.iceflow.MB = zeros(Float64,nx,ny)
+    model.iceflow.MB_mask = zeros(Float64,nx,ny)
+    model.iceflow.MB_total = zeros(Float64,nx,ny)
+
+    params.solver.tstops = Huginn.define_callback_steps(params.simulation.tspan, params.solver.step)
+    stop_condition(u,t,integrator) = Sleipnir.stop_condition_tstops(u,t,integrator, params.solver.tstops) #closure
+    
+    function action!(integrator)
+        if params.simulation.use_MB 
+            # Compute mass balance
+            MB_timestep!(model, glacier, params.solver.step, integrator.t)
+            apply_MB_mask!(integrator.u, glacier, model.iceflow)
+        end
+    end
+
+    cb_MB = DiscreteCallback(stop_condition, action!)
+
+    function SIA2D(H, p, t)
+        simulation=p[1]
+        x=p[2]
+    
+        # function SIA2D(H, SIA2Dmodel)
+            # Retrieve parameters
+            SIA2D_model = simulation.model.iceflow
+            glacier = simulation.glaciers[simulation.model.iceflow.glacier_idx[]]
+            params = simulation.parameters
+
+            # Retrieve parameters
+            B = glacier.B
+            Δx = glacier.Δx
+            Δy = glacier.Δy
+            A = x[1]
+            n = SIA2D_model.n
+            ρ = params.physical.ρ
+            g = params.physical.g
+
+            # Update glacier surface altimetry
+            S = B .+ H
+            
+            # All grid variables computed in a staggered grid
+            # Compute surface gradients on edges
+            dSdx = Huginn.diff_x(S) ./ Δx
+            dSdy = Huginn.diff_y(S) ./ Δy
+            ∇S = (Huginn.avg_y(dSdx).^2 .+ Huginn.avg_x(dSdy).^2).^((n[] - 1)/2) 
+            
+            Γ = 2.0 * A[] * 1e-17  * (ρ * g)^n[] / (n[]+2) # 1 / m^3 s 
+            D = Γ .* Huginn.avg(H).^(n[] + 2) .* ∇S
         
-        return weighted_average_mse
+            # Compute flux components
+            @views dSdx_edges = Huginn.diff_x(S[:,2:end - 1]) ./ Δx
+            @views dSdy_edges = Huginn.diff_y(S[2:end - 1,:]) ./ Δy
         
+            # Cap surface elevaton differences with the upstream ice thickness to 
+            # imporse boundary condition of the SIA equation
+            # We need to do this with Tullio or something else that allow us to set indices.
+            η₀ = 1.0
+            dSdx_edges .= @views @. min(dSdx_edges,  η₀ * H[2:end, 2:end-1]/Δx)
+            dSdx_edges .= @views @. max(dSdx_edges, -η₀ * H[1:end-1, 2:end-1]/Δx)
+            dSdy_edges .= @views @. min(dSdy_edges,  η₀ * H[2:end-1, 2:end]/Δy)
+            dSdy_edges .= @views @. max(dSdy_edges, -η₀ * H[2:end-1, 1:end-1]/Δy)
+        
+            Fx = .-Huginn.avg_y(D) .* dSdx_edges
+            Fy = .-Huginn.avg_x(D) .* dSdy_edges 
+        
+            #  Flux divergence
+            @tullio dH[i,j] := -(Huginn.diff_x(Fx)[pad(i-1,1,1),pad(j-1,1,1)] / Δx + Huginn.diff_y(Fy)[pad(i-1,1,1),pad(j-1,1,1)] / Δy) 
+        
+            return dH
     end
     
-    initial_values = [4.0e-17,3.0]  
-    lower_bound = [8.5e-20, 2.5]    # Adjust as needed
-    upper_bound = [8.0e-17, 4.2]    # Adjust as needed
-    p = (glacier_idx,simulation)
+    realsol = glacier.H_glathida
+    plot_glacier(glacier, "heatmaps", [:H_glathida])
+    
 
-    # TODO: fix right optimizing method
-    optf = OptimizationFunction(objective_function,  Optimization.AutoZygote())
-    prob = OptimizationProblem(optf, initial_values, p,  lb = lower_bound,ub = upper_bound)
+    function objfun(x, simulation, realsol)
+        
+        iceflow_prob = ODEProblem{false}(SIA2D, model.iceflow.H, params.simulation.tspan, tstops=params.solver.tstops, (simulation,x))
+        iceflow_sol = solve(iceflow_prob, 
+                        params.solver.solver, 
+                        callback=cb_MB, 
+                        tstops=params.solver.tstops, 
+                        reltol=params.solver.reltol, 
+                        abstol=params.solver.reltol,
+                        save_everystep=params.solver.save_everystep, 
+                        progress=params.solver.progress, 
+                        progress_steps=params.solver.progress_steps)
 
-    # Import a solver package and solve the optimization problem
-    sol = Optimization.solve(prob, Optim.BFGS())  
+        
+        sol = iceflow_sol.u[end]        
+        map!(x -> ifelse(x>0.0,x,0.0), sol, sol)
+        
+        ofv = 0.0
+        if any((s.retcode != :Success for s in iceflow_sol))
+            ofv = 1e12
+        else
+            ofv = mean((sol[realsol .!= 0] .- realsol[realsol .!= 0]) .^ 2)
+            
+        end
 
-    optimized_A = sol[1]
-   
-    # TODO: change these to optimized values
-    optimized_n = sol[2]
+        
+        return ofv
+    end
+
+    fn(x, p) = objfun(x, p[1], p[2])
+
+    
+    lower_bound = [0.0085]    
+    upper_bound = [8.0] 
+    
+    optfun = OptimizationFunction(fn, Optimization.AutoForwardDiff())
+    optprob = OptimizationProblem(optfun, [4.9], (simulation, realsol), lb = lower_bound,ub = upper_bound)
+    sol = solve(optprob, BFGS(), x_tol=1.0e-3, f_tol = 1e-3)
+
+    optimized_n = 3.0
     optimized_C = 0.0
 
-    inversion_params = InversionParams(optimized_A, optimized_n, optimized_C)
+    inversion_params = InversionParams(sol[1], optimized_n, optimized_C)
+    
     return inversion_params
 end
-
-
-
 
