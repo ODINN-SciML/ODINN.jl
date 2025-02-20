c923b452f415e3faa507641c00b59811f0ae9bef
Jordi Bolibar <jordi.bolibar@gmail.com>
2025-02-20 11:50:54 +0100

    Update branch for docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -41,7 +41,7 @@ makedocs(
 deploydocs(
     repo = "github.com/ODINN-SciML/ODINN.jl",
     branch = "gh-pages",
-    devbranch = "docs",
+    devbranch = "new_API",
     push_preview = true,
     forcepush = true,
 )
