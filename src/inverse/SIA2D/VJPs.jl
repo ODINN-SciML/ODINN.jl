f7d6eadcb2cc009ab671f9c0edb9f33949274f3a
Facundo Sapienza <fsapienza@berkeley.edu>
2025-04-16 11:54:26 -0700

    Move discrete VJP code and tests to ODINN
    Co-authored-by: albangossard <alban.paul.gossard@gmail.com>

---
+++ src/inverse/SIA2D/VJPs.jl
@@ -0,0 +1,65 @@
+
+function VJP_λ_∂SIA∂H(VJPMode::DiscreteVJP, λ, H, θ, simulation, t, batch_id)
+    λ_∂f∂H = VJP_λ_∂SIA_discrete(λ, H, θ, simulation, t; batch_id = batch_id)[1]
+    return λ_∂f∂H, nothing
+end
+
+function VJP_λ_∂SIA∂H(VJPMode::ContinuousVJP, λ, H, θ, simulation, t, batch_id)
+    λ_∂f∂H = VJP_λ_∂SIA∂H_continuous(λ, H, simulation, t; batch_id = batch_id)
+    return λ_∂f∂H, nothing
+end
+
+function VJP_λ_∂SIA∂H(VJPMode::EnzymeVJP, λ, H, θ, simulation, t, batch_id)
+    dH_H = Enzyme.make_zero(H)
+    λ_∂f∂H = Enzyme.make_zero(H)
+    _simulation = Enzyme.make_zero(simulation)
+    smodel = StatefulLuxLayer{true}(simulation.model.machine_learning.architecture, θ.θ, simulation.model.machine_learning.st)
+
+    λH = deepcopy(λ) # Need to copy because Enzyme changes the backward gradient in-place
+    Enzyme.autodiff(
+        Reverse, SIA2D_adjoint!, Const,
+        Enzyme.Const(θ),
+        Duplicated(dH_H, λH),
+        Duplicated(H, λ_∂f∂H),
+        Enzyme.Duplicated(simulation, _simulation),
+        Enzyme.Const(smodel),
+        Enzyme.Const(t),
+        Enzyme.Const(batch_id)
+    )
+    return λ_∂f∂H, dH_H
+end
+
+function VJP_λ_∂SIA∂θ(VJPMode::DiscreteVJP, λ, H, θ, dH_H, dH_λ, simulation, t, batch_id)
+    λ_∂f∂θ = VJP_λ_∂SIA_discrete(λ, H, θ, simulation, t; batch_id = batch_id)[2]
+    return λ_∂f∂θ
+end
+
+function VJP_λ_∂SIA∂θ(VJPMode::ContinuousVJP, λ, H, θ, dH_H, dH_λ, simulation, t, batch_id)
+    λ_∂f∂θ = VJP_λ_∂SIA∂θ_continuous(θ, λ, H, simulation, t; batch_id = batch_id)
+    return λ_∂f∂θ
+end
+
+function VJP_λ_∂SIA∂θ(VJPMode::EnzymeVJP, λ, H, θ, dH_H, dH_λ, simulation, t, batch_id)
+    λ_∂f∂θ = Enzyme.make_zero(θ)
+    _simulation = Enzyme.make_zero(simulation)
+    smodel = StatefulLuxLayer{true}(simulation.model.machine_learning.architecture, θ.θ, simulation.model.machine_learning.st)
+    _smodel = Enzyme.make_zero(smodel)
+    _H = Enzyme.make_zero(H)
+
+    λθ = deepcopy(λ) # Need to copy because Enzyme changes the backward gradient in-place
+    Enzyme.autodiff(
+        Reverse, SIA2D_adjoint!, Const,
+        Duplicated(θ, λ_∂f∂θ),
+        Duplicated(dH_λ, λθ),
+        Duplicated(H, _H),
+        Duplicated(simulation, _simulation),
+        Duplicated(smodel, _smodel),
+        Const(t),
+        Const(batch_id)
+    )
+    # Run simple test that both closures are computing the same primal
+    if !isnothing(dH_H)
+        @assert dH_H ≈ dH_λ "Result from forward pass needs to coincide for both closures when computing the pullback."
+    end
+    return λ_∂f∂θ
+end
---
+++ test/test_utils.jl
@@ -0,0 +1,95 @@
+# export compute_numerical_gradient, stats_err_arrays, printVecScientific
+
+"""
+    compute_numerical_gradient(
+        x,
+        args,
+        fct::Function,
+        ϵ::F;
+        varStr::String = ""
+    ) where {T, F <: AbstractFloat}
+
+Compute the gradient of a function by using numerical differences. The function is
+evaluated N+1 times with N the length of x.
+The function has two arguments: the variable with respect to which the gradient is
+computed and extra arguments which are not differentiated.
+
+Arguments:
+- `x`: Variable x where to evaluate the gradient.
+- `args`: Extra arguments.
+- `fct::Function`: Function to differentiate.
+- `epsilon::F`: Size of perturbation to use in the numerical differences.
+- `varStr::String`: Variable to print in the progress bar. Example: "of A" will
+    print "Computing gradient of A using finite differences with..."
+
+Returns:
+- `grad`: Numerical gradient.
+"""
+function compute_numerical_gradient(
+    x,
+    args,
+    fct::Function,
+    ϵ::F;
+    varStr::String = ""
+) where {F <: AbstractFloat}
+    grad = zero(x)
+    grad_vec = vec(grad) # Points to the same position in memory
+    x_ϵ = deepcopy(x)
+    x_ϵ_vec = vec(x_ϵ)
+    f0 = fct(x, args)
+    show_progress = !parse(Bool, get(ENV, "CI", "false"))
+    pbar = Progress(length(x); desc="Computing gradient $(varStr) using finite differences with ϵ=$(@sprintf("%.1e", ϵ))...", enabled=show_progress)
+    for i in range(1,length(x))
+        x_ϵ .= x
+        x_ϵ_vec[i] += ϵ
+        grad_vec[i] = (fct(x_ϵ, args) - f0) / ϵ
+        next!(pbar)
+    end
+    return grad
+end
+
+"""
+    stats_err_arrays(a::T, b::T) where T
+
+Compute the ratio, the angle and the relative error between two arrays.
+The norm and scalar product are defined in the vectorial sense, meaning that this is
+mathematically equivalent to flatten the arrays.
+The arrays must be of the same type and have the same shape.
+
+Arguments:
+- `a::T`: First array.
+- `b::T`: Second array to compare to the first one.
+
+Returns:
+- `ratio::Float64`: Ratio between the norm of the two arrays minus 1. Value close
+    to zero means the arrays have approximately the same norm.
+- `angle::Float64`: Scalar product between the two arrays normalized by the norm
+    minus 1. Value close to zero means the arrays point towards the same direction.
+- `relerr::Float64`: Relative error between the two arrays. Value close to zero
+    means the arrays have approximately the same values. The norm of `a` is taken
+    to normalize and compute the relative error.
+"""
+function stats_err_arrays(a::T, b::T) where T
+    ratio = sqrt(sum(a.^2)) / sqrt(sum(b.^2)) - 1
+    angle = sum(a.*b) / (sqrt(sum(a.^2)) * sqrt(sum(b.^2))) - 1
+    relerr = sqrt(sum((a - b).^2)) / sqrt(sum((a).^2))
+    return ratio, angle, relerr
+end
+
+printVecScientific(v) = join([@sprintf("%9.2e", e) for e in v], " ")
+function printVecScientific(baseVarName, v, thres=nothing)
+    print(baseVarName)
+    for e in v
+        if isnothing(thres)
+            print(@sprintf("%9.2e", e))
+        else
+            if abs(e)<=thres
+                printstyled(@sprintf("%9.2e", e); color=:green)
+            else
+                printstyled(@sprintf("%9.2e", e); color=:red)
+            end
+        end
+        print(" ")
+    end
+    println("")
+end
