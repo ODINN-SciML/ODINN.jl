dbf707020616f0c7514d4748d6945b85e2c33351
Alban Gossard <alban.paul.gossard@gmail.com>
2025-04-22 16:55:38 +0200

    Functional inversion tutorial (#304)
    * [WIP] Functional inversion tutorial
    
    I'm having some issues with making the multiprocessing work with the tutorial. I'm getting a strange error which I cannot fix. I'll leave it here for now and I'll open an issue to track this.
    
    * [WIP] Debugging documentation problems
    
    * [WIP] More debugging of multiple tutorials together
    
    * [WIP] Removing Manifest for docs env
    
    * [WIP] Debugging both tutorials with multiprocessing
    
    * [WIP] Missing files
    
    * [WIP] test with only functional inversion and less epochs
    
    * Testing both tutorials together
    
    * fix documentation generation
    
    * switch to MacOS for documentation generation
    
    * fix documentation generation by turning off multiprocessing with ODINN_OVERWRITE_MULTI
    
    * minor changes in the CI + update Huginn version in Project.toml
    
    ---------
    
    Co-authored-by: Jordi <jordi.bolibar@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -11,6 +11,8 @@ Pkg.activate(".")
 Pkg.develop(PackageSpec(path=".."))
 Pkg.instantiate()
 
+ENV["ODINN_OVERWRITE_MULTI"] = true
+
 using Revise
 using Documenter, Literate
 using ODINN
@@ -18,26 +20,37 @@ include("src/doc_utils.jl")
 
 DocMeta.setdocmeta!(ODINN, :DocTestSetup, :(using ODINN); recursive=true)
 
-# Convert tutorial/examples to markdown
-Literate.markdown("./src/tutorials.jl", "./src";
-                  name = "tutorials", preprocess = replace_includes)
+# List of tutorial files
+tutorial_files = [
+    "./src/forward_simulation.jl",
+    "./src/functional_inversion.jl"
+]
+
+# Generate independent Markdown files for each tutorial
+for tutorial_file in tutorial_files
+    tutorial_name = splitext(basename(tutorial_file))[1]  # Extract the file name without extension
+    Literate.markdown(tutorial_file, "./src"; name = tutorial_name)
+end
 
 # Which markdown files to compile to HTML
 makedocs(
     modules=[ODINN, Huginn, Muninn, Sleipnir],
-    authors="Jordi Bolibar, Facu Sapienza",
-    repo="https://github.com/ODINN-SciML/ODINN.jl/blob/{commit}{path}#{line}",
+    authors="Jordi Bolibar, Facu Sapienza, Alban Gossard",
+    repo=Remotes.GitHub("ODINN-SciML", "ODINN.jl"),
     sitename="ODINN.jl",
-    format=Documenter.HTML(
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://ODINN-SciML.github.io/ODINN.jl",
-        assets=String[]
-        # size_threshold=500 * 1024,  # Increase size threshold to 500 KiB
-        # size_threshold_warn=250 * 1024  # Increase warning threshold to 250 KiB
+    format = Documenter.HTML(
+        prettyurls=get(ENV, "CI", nothing)=="true",
+        ansicolor=true, collapselevel=1,
+        size_threshold=2000 * 1024,  # Increase size threshold to 500 KiB
+        size_threshold_warn=1000 * 1024,  # Increase warning threshold to 250 KiB),      # in bytes
+        example_size_threshold=1000 * 1024
     ),
     pages=[
         "Home" => "index.md",
-        "Tutorials" => "tutorials.md",
+        "Tutorials" => [
+            "Forward simulation" => "forward_simulation.md",
+            "Functional inversion" => "functional_inversion.md"
+        ],
         "Types and functions" => "funcs_types.md",
         "API" => "api.md"
     ],
