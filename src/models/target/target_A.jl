cb77f76827eee0fe61390751564ca534bd99af05
Alban Gossard <alban.paul.gossard@gmail.com>
2025-06-04 14:14:29 +0200

    add basal sliding in SIA

--- src/models/target/target_A.jl
+++ src/models/target/target_A.jl
@@ -23,27 +23,32 @@ function Diffusivity(
     H, ∇S, θ, iceflow_model, ml_model, glacier, params
     )
     n = iceflow_model.n
+    C = iceflow_model.C
     Γ_no_A = Γ(iceflow_model, params; include_A = false)
     A = apply_parametrization(
         target;
         H = H, ∇S = ∇S, θ = θ,
         iceflow_model = iceflow_model, ml_model = ml_model, glacier = glacier, params = params
     )
-    return A .* Γ_no_A .* H.^(n[] + 2) .* ∇S.^(n[] - 1)
+    return (C .+ A .* Γ_no_A .* H) .* H.^(n[] + 1) .* ∇S.^(n[] - 1)
 end
 
 function ∂Diffusivity∂H(
     target::SIA2D_A_target;
     H, ∇S, θ, iceflow_model, ml_model, glacier, params
     )
-    return Γ(iceflow_model, params) .* (iceflow_model.n[] + 2) .* H.^(iceflow_model.n[] + 1) .* ∇S.^(iceflow_model.n[] - 1)
+    n = iceflow_model.n
+    C = iceflow_model.C
+    return ( C .* (n[] + 1) .+ Γ(iceflow_model, params) .* H .* (n[] + 2) ) .* H.^n[] .* ∇S.^(n[] - 1)
 end
 
 function ∂Diffusivity∂∇H(
     target::SIA2D_A_target;
     H, ∇S, θ, iceflow_model, ml_model, glacier, params
     )
-    return Γ(iceflow_model, params) .* (iceflow_model.n[] - 1) .* H.^(iceflow_model.n[] + 2) .* ∇S.^(iceflow_model.n[] - 3)
+    n = iceflow_model.n
+    C = iceflow_model.C
+    return ( C .+ Γ(iceflow_model, params) .* H ) .* (n[] - 1) .* H.^(n[] + 1) .* ∇S.^(n[] - 3)
 end
 
 function ∂Diffusivity∂θ(
--- test/runtests.jl
+++ test/runtests.jl
@@ -50,13 +50,14 @@ ENV["GKSwstype"] = "nul"
     @testset "Adjoint of diff" test_adjoint_diff()
     @testset "Adjoint of clamp_borders" test_adjoint_clamp_borders()
     @testset "Adjoint of avg" test_adjoint_avg()
-    # @testset "Adjoint of SIA2D" test_adjoint_SIAD2D()
 end
 
 @testset "Adjoint method of SIA equation with A as target" begin
     # @testset "VJP (Enzyme) of SIA2D vs finite differences" test_adjoint_SIA2D(ContinuousAdjoint(VJP_method = ODINN.EnzymeVJP()); target = :A) # This test must be run first, otherwise Enzyme compilation fails because it was used before
     @testset "VJP (discrete) of SIA2D vs finite differences" test_adjoint_SIA2D(ContinuousAdjoint(VJP_method = DiscreteVJP()); target = :A)
+    @testset "VJP (discrete) of SIA2D with C>0 vs finite differences" test_adjoint_SIA2D(ContinuousAdjoint(VJP_method = DiscreteVJP()); target = :A, C=5e4)
     @testset "VJP (continuous) of SIA2D vs finite differences" test_adjoint_SIA2D(ContinuousAdjoint(VJP_method = ContinuousVJP()); target = :A)
+    @testset "VJP (continuous) of SIA2D with C>0 vs finite differences" test_adjoint_SIA2D(ContinuousAdjoint(VJP_method = ContinuousVJP()); thres=[5e-4, 7e-4, 4e-2], target = :A, C=5e4)
     @testset "Manual implementation of the discrete adjoint with discrete VJP vs finite differences" test_grad_finite_diff(DiscreteAdjoint(VJP_method = DiscreteVJP()); thres = [1e-2, 1e-5, 1e-2])
     @testset "Manual implementation of the discrete adjoint with continuous VJP vs finite differences" test_grad_finite_diff(DiscreteAdjoint(VJP_method = ContinuousVJP()); thres = [2e-2, 1e-5, 2e-2])
     @testset "Manual implementation of the continuous adjoint with discrete VJP vs finite differences" test_grad_finite_diff(ContinuousAdjoint(VJP_method = DiscreteVJP()); thres = [1e-2, 1e-5, 1e-2])
