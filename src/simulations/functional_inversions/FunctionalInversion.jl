70b1b31b93fcf0ce07c9cc0634fd4b939c84de06
Alban Gossard <alban.paul.gossard@gmail.com>
2025-06-29 23:54:13 +0200

    refactor MLmodel to allow having multiple regressors

--- src/simulations/functional_inversions/FunctionalInversion.jl
+++ src/simulations/functional_inversions/FunctionalInversion.jl
@@ -47,6 +47,9 @@ function FunctionalInversion(
     # We perform this check here to avoid having to provide the parameters when creating the model
     @assert targetType(model.machine_learning.target) == parameters.UDE.target "Target does not match the one provided in the parameters."
 
+    # We perform this check here to avoid having to provide the parameters when creating the model
+    @assert targetType(model.machine_learning.target) == parameters.UDE.target "Target does not match the one provided in the parameters."
+
     # Build the results struct based on input values
     functional_inversion = FunctionalInversion{cache_type(model)}(model, nothing,
                             glaciers,
