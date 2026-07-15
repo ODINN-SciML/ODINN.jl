bc33962f736be72b0bed7e1572dbd0022bd7d3a0
Alban Gossard <alban.paul.gossard@gmail.com>
2026-07-15 14:08:05 +0200

    switch to Julia 1.11 for full tests and split them

--- test/runtests.jl
+++ test/runtests.jl
@@ -279,7 +279,7 @@ ENV["GKSwstype"] = "nul"
         @testset "Multiglacier inversion test" begin
             @testset "Continuous adjoint with discrete VJP vs finite differences" test_grad_finite_diff(
                 ContinuousAdjoint(VJP_method = DiscreteVJP());
-                thres = [1e-4, 1e-8, 1e-4], multiglacier = true)
+                thres = [2e-4, 1e-8, 2e-4], multiglacier = true)
             @testset "Continuous adjoint with discrete VJP vs finite differences (initial condition)" test_grad_finite_diff(
                 ContinuousAdjoint(VJP_method = DiscreteVJP()); thres = [1e-3, 1e-8, 1e-3],
                 multiglacier = true, train_initial_conditions = true)
