cf9e222943ed9fde9fabd912954e376c474293ed
Alban Gossard <alban.paul.gossard@gmail.com>
2026-08-29 16:36:08 +0200

    tighten thresholds

--- test/runtests.jl
+++ test/runtests.jl
@@ -128,7 +128,7 @@ ENV["GKSwstype"] = "nul"
                 DiscreteAdjoint(VJP_method = DiscreteVJP());
                 thres = [5e-3, 5e-7, 5e-3], train_initial_conditions = true)
             @testset "Discrete adjoint with continuous VJP vs finite differences" test_grad_finite_diff(
-                DiscreteAdjoint(VJP_method = ContinuousVJP()); thres = [2e-4, 1e-8, 2e-4])
+                DiscreteAdjoint(VJP_method = ContinuousVJP()); thres = [1e-4, 1e-8, 1e-4])
             @testset "Continuous adjoint with discrete VJP vs finite differences" test_grad_finite_diff(
                 ContinuousAdjoint(VJP_method = DiscreteVJP()); thres = [1e-3, 1e-8, 1e-3])
             @testset "Continuous adjoint with discrete VJP vs finite differences (initial condition)" test_grad_finite_diff(
@@ -157,12 +157,13 @@ ENV["GKSwstype"] = "nul"
             # vector of MB models flowing through the VJPs.
             @testset "Continuous adjoint w/ discrete MB VJP and calibrated MB" test_grad_finite_diff(
                 ContinuousAdjoint(VJP_method = DiscreteVJP(), MB_VJP = DiscreteVJP());
-                thres = [3e-3, 1e-8, 3e-3], use_MB = true, calibrate_MB = true)
+                thres = [2e-3, 1e-10, 2e-3], use_MB = true, calibrate_MB = true)
             @testset "Continuous adjoint with continuous VJP vs finite differences" test_grad_finite_diff(
-                ContinuousAdjoint(VJP_method = ContinuousVJP()); thres = [2e-2, 1e-5, 2e-2])
+                ContinuousAdjoint(VJP_method = ContinuousVJP()); thres = [
+                    5e-3, 1e-10, 5e-3])
             @testset "Continuous adjoint with Enzyme VJP vs finite differences" test_grad_finite_diff(
                 ContinuousAdjoint(VJP_method = ODINN.EnzymeVJP());
-                thres = [5e-4, 7e-7, 2e-3])
+                thres = [5e-4, 1e-10, 5e-4])
             @testset "SciMLSensitivity adjoint with Enzyme VJP vs finite differences" test_grad_finite_diff(
                 ODINN.SciMLSensitivityAdjoint(); thres = [1e-5, 1e-13, 1e-5])
             @testset "SciMLSensitivity auto-adjoint vs manual ContinuousAdjoint for LawA" test_grad_sciml_vs_manual(thres = [
