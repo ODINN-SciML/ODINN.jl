60cc90a7b2a8ea549ad5b892e92bd9385414e94e
Documenter.jl <documenter@juliadocs.github.io>
2026-09-14 09:59:47 +0000

    build based on 9d143ed

---
+++ previews/PR274/doc_utils.jl
@@ -0,0 +1,14 @@
+function replace_includes(str)
+
+    included = ["forward_simulation.jl"] # TODO: add new tutorials as they come
+
+    # Here the path loads the files from their proper directory,
+    # which may not be the directory of the `examples.jl` file!
+    path = "src/tutorials/"
+
+    for ex in included
+        content = read(path*ex, String)
+        str = replace(str, "include(\"$(ex)\")" => content)
+    end
+    return str
+end
